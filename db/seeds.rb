0bc62c07d98234f184bdc1c489ecf071527f765b
Adrien Hors <adrienhors1@gmail.com>
2022-08-17 16:12:51 +0200

    Rails Simple Airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Cozy room in the 5th district',
+  address: '10 rue Tournefort',
+  description: 'A lovely summer feel for this luminous studio. One double bedroom, open plan living area, large kitchen',
+  price_per_night: 50,
+  number_of_guests: 1
+)
